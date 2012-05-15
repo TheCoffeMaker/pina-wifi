48835d47995c4607e2050a17c3b5a2687891b5ef
Andréz Lamouroux <alamouroux@gmail.com>
2012-05-14 21:49:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
-<head>
-<meta http-equiv="REFRESH" content="0;url=redirect.php">
-</head>
-<body>
-</body>
+  <head>
+    <meta http-equiv="REFRESH" content="0;url=redirect.php">
+  </head>
+  <body>
+  </body>
 </html>
 
 
